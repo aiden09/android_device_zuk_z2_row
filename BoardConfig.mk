d797dc3d1f42a11bb990ea052ec7400ce3b0ef28
HostZero <haneesh.da.rocker@gmail.com>
2016-09-12 18:10:37 +0530

    z2_row: use custom kernel tags ramdisk offset

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,8 @@ TARGET_BOARD_INFO_FILE := device/zuk/z2_row/board-info.txt
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_z2_row_defconfig
 TARGET_KERNEL_SOURCE := kernel/zuk/msm8996
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET     := 0x01000000
 
 # inherit from proprietary files
 -include vendor/zuk/z2_row/BoardConfigVendor.mk
